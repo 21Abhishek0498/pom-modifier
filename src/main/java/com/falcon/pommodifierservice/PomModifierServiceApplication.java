3456fbb420652ed52fbbccb91da1d02eb6694a01
21Abhishek0498 <abhishekgorakh2104@gmail.com>
2023-11-24 13:14:37 +0530

    shanges

---
+++ src/main/java/com/falcon/pommodifierservice/PomModifierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.falcon.pommodifierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomModifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomModifierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falcon/pommodifierservice/PomModifierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon.pommodifierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomModifierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
